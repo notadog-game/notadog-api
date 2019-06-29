5713e0d541d85583a205546ad847c314fb2034f3
Sergey Perezhogin <sperezhogin@exadel.com>
2019-06-29 20:21:09 +0500

    Initial

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotadogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "user1", "user2" };
+        }
+    }
+}
